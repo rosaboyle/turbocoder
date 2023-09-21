4de10b47c4e4a8222866d996285b9e4be2e8c8e6
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-09-21 18:28:42 -0400

    Create dependency_gen.py

---
+++ utils/deps/dependency_gen.py
@@ -0,0 +1,43 @@
+
+
+
+
+import os
+import json
+import openai
+# openai.api_key = ""
+prompt = """Here is the code. I don't know what language it is written in. I want you to find the language it is written in. Then get the dependency file for the code. The output should be in the format of json with keys "lang", "depsFileName","fileContent"
+
+{}
+
+I just want you to give me the json and nothing else. Don't explain me. Don't tell me anything else.
+"""
+
+def get_prompt(code):
+    return prompt.format(code)
+
+def is_valid_json(json_str):
+    required_keys = ["lang", "depsFileName", "fileContent"]
+    
+    try:
+        # Parse the JSON string
+        data = json.loads(json_str)
+        
+        # Check if all required keys are present
+        if all(key in data for key in required_keys):
+            return True
+        else:
+            return False
+    except json.JSONDecodeError:
+        return False
+      
+def get_deps(code):
+  prompt_in = prompt.format(code)
+  completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt_in}
+  ]
+  )
+  return completion.choices[0].message.content
