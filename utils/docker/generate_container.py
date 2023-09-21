7943238dd5394e8e47a3ebc96d294453636d5873
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-09-21 16:38:32 -0400

    Create generate_container.py

---
+++ utils/docker/generate_container.py
@@ -0,0 +1,23 @@
+import docker
+
+def run_container(image_name):
+    client = docker.from_env()
+    
+    # Check if the container with the given image name is already running
+    containers = client.containers.list(filters={"ancestor": image_name})
+    if containers:
+        print(f"Container with image {image_name} is already running.")
+        return
+
+    # Run a new container with the dummy command to keep it running indefinitely
+    container = client.containers.run(
+        image=image_name,
+        command="tail -f /dev/null",
+        detach=True,  # Run in detached mode
+        restart_policy={"Name": "always"}  # Restart always policy
+    )
+    print(f"Started container with ID {container.id} using image {image_name}.")
+
+if __name__ == "__main__":
+    image_name = input("Enter the Docker image name you want to keep running: ")
+    run_container(image_name)
